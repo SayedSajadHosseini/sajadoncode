f183ca861da753ca5caece4af2688f42349fb4ef
sajadoncode <sajadoncode@gmail.com>
2023-03-02 21:59:45 +0100

    fetch github api

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,18 +1,30 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function Projects(props) {
+  const [repos, setRepos] = useState([]);
+  useEffect(() => {
+    function getRepos(username) {
+      return fetch(`https://api.github.com/users/${username}/repos`)
+        .then((response) => response.json())
+        .then((result) => setRepos(result))
+        .catch((err) => console.log(err.message));
+    }
+    getRepos("SayedSajadHosseini");
+  }, []);
+
   return (
     <div>
-      <h3 className="pt-5 font-bold">Open Source work</h3>
-      <ul className="pt-2">
-        <li className="pt-2">
-          <a href="https://github.com/SayedSajadHosseini/Dayn-Calendar---js">
-            Calendar
-          </a>
-        </li>
-        <li className="pt-2">
-          <a href="https://github.com/SayedSajadHosseini/metaxa">Metaxa</a>
-        </li>
+      <h3 className="pt-5 font-bold text-2xl mb-2 text-yellow-500">
+        Open Source work
+      </h3>
+      <ul className="space-y-1 pl-3">
+        {repos.map((repo) => (
+          <li className="hover:text-yellow-600" key={repo.id}>
+            <a href={repo.svn_url}>{repo.name}</a>
+          </li>
+        ))}
       </ul>
     </div>
   );
