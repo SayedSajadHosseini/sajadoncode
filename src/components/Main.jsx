71b0537c362f27777b8e6afb6e64be0db18d66c8
sajadoncode <sajadoncode@gmail.com>
2023-03-02 21:58:53 +0100

    remove unless code

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,23 +2,16 @@ import React from "react";
 
 function Main(props) {
   return (
-    <div className="text-gray-500">
-      <p className="pt-5 text-justify">
-        Hi, I'm Sayed Sajad Hosseini, a minimalist web developer who focuses on
-        creating clean, functional, and user-friendly websites. I believe that
-        less is more! and my designs reflect that ethos. 😗
-        {/* My approach to web
-        development is to strip away the unnecessary elements and focus on what
-        really matters – providing a seamless user experience. I have expertise
-        in HTML, CSS, JavaScript, and various front-end frameworks, and I stay
-        up-to-date with the latest trends and technologies in the industry. My
-        goal is to create websites that not only look great but also perform
-        well, are easy to navigate, and load quickly. When I'm not coding, I
-        enjoy spending time outdoors, practicing yoga, and reading. I'm
-        passionate about mindfulness and living a simple, intentional life, and
-        I bring that same philosophy to my work as a web developer. If you're
-        looking for a web developer who values simplicity, functionality, and
-        aesthetics, let's connect and discuss your project! */}
+    <div className="">
+      <p className="pt-5">
+        <span className="text-2xl font-semibold text-yellow-500">
+          Hi, I'm Sayed Sajad Hosseini,
+        </span>
+        <p className="mt-2">
+          A minimalist web developer who focuses on creating clean, functional,
+          and user friendly websites.
+          <span className="block">I believe that less is more! 😗</span>
+        </p>
       </p>
       <p className="pt-5">
         I care about developer experience and wanna save efforts for development
